b5136ffcaef415cc1256ba9229ef0019ec62d1dc
Sabelo Mk <90407948+LaplaceXero@users.noreply.github.com>
2023-02-03 17:03:43 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int testCases = 0;
+	int x; //distance to office
+	
+	cin >>testCases;
+	
+	for(int i = 0; i < testCases; i++)
+	{
+	    cin >>x;
+	    
+	    cout <<(x * 10) <<endl;
+	}
+	
+	
+	return 0;
+}
